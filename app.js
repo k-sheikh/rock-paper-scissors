e22b5e79409397a1664d6fc25003046fe37a1a92
Kashan Sheikh <k-sheikh@hotmail.com>
2022-07-15 23:52:21 +0100

    feat: Add logic for a single round game

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 console.log("Rock, Paper, Scissors");
 
-const myArray = ["Rock", "Paper", "Scissors"];
+const myArray = ["rock", "paper", "scissors"];
 
 function computerPlay() {
 	return myArray[Math.floor(Math.random() * myArray.length)];
+}
+
+let playerSelection;
+let computerSelection;
+
+function playRound(playerSelection, computerSelection) {
+	playerSelection = playerSelection.toLowerCase();
+	computerSelection = computerPlay().toLowerCase();
+
+	if (playerSelection == computerSelection) {
+		return `Tie game.`;
+	} else if (
+		(playerSelection == "rock" && computerSelection == "scissors") ||
+		(playerSelection == "paper" && computerSelection == "rock") ||
+		(playerSelection == "scissors" && computerSelection == "paper")
+	) {
+		return `You win, ${playerSelection} beats ${computerSelection}!`;
+	} else {
+		return `You lose, ${computerSelection} beats ${playerSelection}!`;
+	}
 }
